6fc362a7bc62934017eebac27b942ae0afa1c88f
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2024-02-27 19:53:25 +0100

    first test for reader module

--- src/main/java/pl/javaready/readerarticleretriever/ReaderArticleRetrieverFacade.java
+++ src/main/java/pl/javaready/readerarticleretriever/ReaderArticleRetrieverFacade.java
@@ -11,7 +11,7 @@ public class ReaderArticleRetrieverFacade {
 //        );
         return ArticleDto.builder()
                 .content("5 kroków: 1, 2 , 3, ,4")
-                .header( "jak zostac programistą")
+                .header("jak zostac programistą")
                 .id(1L)
                 .category("IT")
                 .build();
--- src/test/java/pl/javaready/readerarticleretriever/ReaderArticleRetrieverFacadeTest.java
+++ src/test/java/pl/javaready/readerarticleretriever/ReaderArticleRetrieverFacadeTest.java
@@ -1,19 +1,23 @@
 package pl.javaready.readerarticleretriever;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ReaderArticleRetrieverFacadeTest {
 
     @Test
-    public void f(){
+    @DisplayName("should return article with id 1 and content xxx")
+    public void should_return_article_with_id_1_and_content_xxx(){
         // given
         ReaderArticleRetrieverFacade facade = new ReaderArticleRetrieverFacade();
         // when
-//        ArticleDto articleDto = facade.retrieveById(1);
         ArticleDto articleDto = facade.retrieveById(1L);
         // then
+        assert articleDto.id().equals(1L);
+        assert articleDto.content().equals("5 kroków: 1, 2 , 3, ,4");
+        assert articleDto.header().equals("jak zostac programistą");
+        assert articleDto.category().equals("IT");
+
     }
 
 }
